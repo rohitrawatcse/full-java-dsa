fc989a5300a864637a9ab5329a6a02fb6bb58654
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2022-11-11 23:40:07 +0530

    Update cl2-sorting.java

--- cl2-sorting.java
+++ cl2-sorting.java
@@ -28,7 +28,7 @@ public class BubbleSortExample {
     
     
     public static void main(String[] args) {  
-                int arr[] ={3,60,35,2,45,320,5};  
+                int arr[] ={3,6,2,1,8,7,4,5,3,1};  
                  
                 System.out.println("Array Before Bubble Sort");  
                 for(int i=0; i < arr.length; i++){  
